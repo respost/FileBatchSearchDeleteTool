6a857822c7f38156c1bca7dee738a69d0bf3f5ff
xiao7 <15577969@qq.com>
2020-11-04 12:51:26 +0800

    上传项目

---
+++ 文件批量搜索删除工具/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace 文件批量搜索删除工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelFileBox = new System.Windows.Forms.Panel();
+            this.labTips = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.AddPath = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDirPath = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.labelUrl = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.panelCopyright = new System.Windows.Forms.Panel();
+            this.panelFileBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AddPath)).BeginInit();
+            this.panelCopyright.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelFileBox
+            // 
+            this.panelFileBox.AllowDrop = true;
+            this.panelFileBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelFileBox.BackColor = System.Drawing.SystemColors.Control;
+            this.panelFileBox.Controls.Add(this.labTips);
+            this.panelFileBox.Controls.Add(this.listView1);
+            this.panelFileBox.Location = new System.Drawing.Point(7, 43);
+            this.panelFileBox.Name = "panelFileBox";
+            this.panelFileBox.Size = new System.Drawing.Size(480, 217);
+            this.panelFileBox.TabIndex = 2;
+            this.panelFileBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.panelFileBox_DragDrop);
+            this.panelFileBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.panelFileBox_DragEnter);
+            // 
+            // labTips
+            // 
+            this.labTips.AutoSize = true;
+            this.labTips.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.labTips.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.labTips.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labTips.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.labTips.Location = new System.Drawing.Point(148, 100);
+            this.labTips.Name = "labTips";
+            this.labTips.Size = new System.Drawing.Size(184, 16);
+            this.labTips.TabIndex = 1;
+            this.labTips.Text = "拖拽要删除的文件到这里";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(480, 217);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // AddPath
+            // 
+            this.AddPath.Image = ((System.Drawing.Image)(resources.GetObject("AddPath.Image")));
+            this.AddPath.Location = new System.Drawing.Point(392, 13);
+            this.AddPath.Name = "AddPath";
+            this.AddPath.Size = new System.Drawing.Size(20, 18);
+            this.AddPath.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.AddPath.TabIndex = 7;
+            this.AddPath.TabStop = false;
+            this.AddPath.Click += new System.EventHandler(this.AddPath_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "文件所在目录：";
+            // 
+            // txtDirPath
+            // 
+            this.txtDirPath.Location = new System.Drawing.Point(100, 12);
+            this.txtDirPath.Name = "txtDirPath";
+            this.txtDirPath.Size = new System.Drawing.Size(313, 21);
+            this.txtDirPath.TabIndex = 6;
+            // 
+            // btnDel
+            // 
+            this.btnDel.BackColor = System.Drawing.Color.Green;
+            this.btnDel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDel.ForeColor = System.Drawing.Color.White;
+            this.btnDel.Location = new System.Drawing.Point(419, 10);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(68, 23);
+            this.btnDel.TabIndex = 9;
+            this.btnDel.Text = "点击删除";
+            this.btnDel.UseVisualStyleBackColor = false;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.Color.Gray;
+            this.txtLog.ForeColor = System.Drawing.Color.White;
+            this.txtLog.Location = new System.Drawing.Point(7, 266);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(480, 99);
+            this.txtLog.TabIndex = 10;
+            // 
+            // labelUrl
+            // 
+            this.labelUrl.AutoSize = true;
+            this.labelUrl.BackColor = System.Drawing.Color.Transparent;
+            this.labelUrl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelUrl.ForeColor = System.Drawing.Color.White;
+            this.labelUrl.Location = new System.Drawing.Point(79, 33);
+            this.labelUrl.Name = "labelUrl";
+            this.labelUrl.Size = new System.Drawing.Size(281, 12);
+            this.labelUrl.TabIndex = 14;
+            this.labelUrl.Text = "开发赞助商：资源驿站（www.zy13.net），点击访问";
+            this.labelUrl.Click += new System.EventHandler(this.labelUrl_Click);
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.BackColor = System.Drawing.Color.Transparent;
+            this.label22.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label22.ForeColor = System.Drawing.Color.White;
+            this.label22.Location = new System.Drawing.Point(37, 12);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(365, 12);
+            this.label22.TabIndex = 13;
+            this.label22.Text = "软件仅供学习和研究，不得用于非法盈利，违者自行承担法律责任。";
+            // 
+            // panelCopyright
+            // 
+            this.panelCopyright.BackColor = System.Drawing.Color.Gray;
+            this.panelCopyright.Controls.Add(this.labelUrl);
+            this.panelCopyright.Controls.Add(this.label22);
+            this.panelCopyright.Location = new System.Drawing.Point(28, 289);
+            this.panelCopyright.Name = "panelCopyright";
+            this.panelCopyright.Size = new System.Drawing.Size(439, 57);
+            this.panelCopyright.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 373);
+            this.Controls.Add(this.panelCopyright);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.AddPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDirPath);
+            this.Controls.Add(this.panelFileBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文件批量搜索删除工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelFileBox.ResumeLayout(false);
+            this.panelFileBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AddPath)).EndInit();
+            this.panelCopyright.ResumeLayout(false);
+            this.panelCopyright.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelFileBox;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox AddPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDirPath;
+        private System.Windows.Forms.Label labTips;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label labelUrl;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Panel panelCopyright;
+    }
+}
+
---
+++ 文件批量搜索删除工具/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace 文件批量搜索删除工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //代理
+        private delegate void SetPos(int ipos, string vinfo);
+        //文件集合
+        private ArrayList fileList = new ArrayList();
+        //目录
+        private string directory;
+        //Ini文件工具
+        private IniFile ini = null;
+        //计数
+        private int num = 0;
+
+        private void PerformTask()
+        {
+            //设置线程池最大线程数量
+            ThreadPool.SetMaxThreads(5, 5);
+            ThreadPool.QueueUserWorkItem(delegate { TaskRun(); });
+        }
+        private void TaskRun()
+        {
+            try
+            {
+                if (fileList != null && fileList.Count > 0)
+                {
+                    for (int i = 0; i < fileList.Count; i++)
+                    {
+                        string filename = fileList[i].ToString();
+                        //MessageBox.Show("删除文件："+filename);
+                        DelLicenseFiles(directory, filename);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.directory = AppDomain.CurrentDomain.BaseDirectory;
+            this.txtDirPath.Text = directory;
+            //读取config.ini配置文件
+            this.ini = new IniFile("config.ini");
+            this.ini.ReadSectionValues("file", fileList);
+            if (fileList != null && fileList.Count > 0)
+            {
+                foreach (string item in fileList)
+                {
+                    //过滤非法字符
+                    string str = Unicode2String(item);
+                    LoadListView(str);
+                }
+            }
+            // 设置行高
+            ImageList imgList = new ImageList();
+            // 分别是宽和高
+            imgList.ImageSize = new Size(1, 25);
+            // 这里设置listView的SmallImageList ,用imgList将其撑大
+            listView1.SmallImageList = imgList;
+            //在各数据之间形成网格线
+            listView1.GridLines = true;
+            //显示列名称
+            listView1.View = View.Details;
+            //不显示滚动条
+            listView1.Columns.Add("文件名（拖拽要删除的文件到列表里）", listView1.Width, HorizontalAlignment.Center);
+            //listView1.Columns.Add("路径", listView1.Width - 100, HorizontalAlignment.Center);
+        }
+
+        private void panelFileBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void LoadListView(string fileName)
+        {
+            this.labTips.Visible = false;
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = fileName;
+            //lvi.SubItems.Add(fullPath);
+            this.listView1.Items.Add(lvi);
+        }
+        private void panelFileBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string fullPath = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            if (!File.Exists(fullPath))
+            {
+                MessageBox.Show("只能拖入文件");
+                return;
+            }
+            string extension = System.IO.Path.GetExtension(fullPath);//扩展名
+            string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fullPath);//文件名
+            string fileName = fileNameWithoutExtension.Trim() + extension.Trim();
+            //string md5 = GetMD5HashFromFile(fullPath);
+            if (fileList.Contains(fileName))
+            {
+                MessageBox.Show("文件已存在");
+                return;
+            }
+            fileList.Add(fileName);
+            LoadListView(fileName);
+            this.ini.WriteString("file", fileNameWithoutExtension, String2Unicode(fileName));
+        }
+        /// <summary>
+        /// 删除指定文件夹下指定文件名的文件
+        /// </summary>
+        /// <param name="url">文件夹地址</param>
+        /// <param name="name">要删除的文件名</param>
+        /// <returns></returns>
+        
+        public void DelLicenseFiles(string path, string name)
+        {
+            try
+            {
+                DirectoryInfo Folder = new DirectoryInfo(path);
+                var files = Folder.GetDirectories();
+                
+                foreach (FileInfo file in Folder.GetFiles())
+                {
+                    if (file.Attributes != FileAttributes.Directory)
+                    {
+                        //Unicode转字符串
+                        name=Unicode2String(name);
+                        if (name == file.Name)
+                        {
+                            file.Delete();
+                            ++num;
+                            SetTextMesssage(0, string.Format("成功删除第[{0}]个文件:{1}", num,file.FullName));
+                            SetTextMesssage(0, "\r\n");
+                        }
+                    }
+
+                }
+                foreach (var dicInfo in Folder.GetDirectories())
+                {
+                    DelLicenseFiles(dicInfo.FullName, name);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }          
+        }
+
+        /// <summary>
+        /// 计算字符串的MD5值
+        /// </summary>
+        /// <param name="sDataIn">字符串</param>
+        /// <returns></returns>
+        public string GetStrMd5(string sDataIn)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytValue, bytHash;
+            bytValue = System.Text.Encoding.UTF8.GetBytes(sDataIn);
+            bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+        // <summary>
+        /// <summary>
+        /// 字符串转Unicode
+        /// </summary>
+        /// <param name="source">源字符串</param>
+        /// <returns>Unicode编码后的字符串</returns>
+        public static string String2Unicode(string source)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(source);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i += 2)
+            {
+                stringBuilder.AppendFormat("\\u{0}{1}", bytes[i + 1].ToString("x").PadLeft(2, '0'), bytes[i].ToString("x").PadLeft(2, '0'));
+            }
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Unicode转字符串
+        /// </summary>
+        /// <param name="source">经过Unicode编码的字符串</param>
+        /// <returns>正常字符串</returns>
+        public static string Unicode2String(string source)
+        {
+            return new Regex(@"\\u([0-9A-F]{4})", RegexOptions.IgnoreCase | RegexOptions.Compiled).Replace(source, x => string.Empty + Convert.ToChar(Convert.ToUInt16(x.Result("$1"), 16)));
+        }
+        /// <summary>
+        /// 输出日志信息
+        /// </summary>
+        /// <param name="ipos"></param>
+        /// <param name="vinfo"></param>
+        private void SetTextMesssage(int ipos, string vinfo)
+        {
+            if (this.InvokeRequired)
+            {
+                SetPos setpos = new SetPos(SetTextMesssage);
+                this.Invoke(setpos, new object[] { ipos, vinfo });
+            }
+            else
+            {
+                this.txtLog.AppendText(vinfo);
+            }
+        }
+
+        private void AddPath_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                this.directory = folder.SelectedPath;
+                this.txtDirPath.Text = this.directory;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (fileList != null && fileList.Count > 0)
+            {
+                num = 0;
+                this.txtLog.Text = "";
+                this.panelCopyright.Visible = false;
+                //创建线程
+                new Thread(new ThreadStart(this.PerformTask)) { IsBackground = true }.Start();
+            }
+            else
+            {
+                MessageBox.Show("请先拖拽要删除的文件到软件上");
+            }
+
+        }
+
+        private void labelUrl_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.zy13.net");
+        }
+
+    }
+
+}
